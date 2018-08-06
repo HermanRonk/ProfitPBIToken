979ec3ffcc9f9db04964b248f8d813f8e64339fe
Herman Ronk <github@hermanronk.nl>
2018-08-06 09:42:06 +0200

    Test

--- base.js
+++ base.js
@@ -1,8 +1,7 @@
 // Genereren AFAS Token voor web autorisatie
 
 function generateWebAuth(ProfitCode) {
-    document.getElementById("AFASForm").submit();
+    //document.getElementById("AFASForm").submit();
     ProfitCode = 0;
-
     document.getElementById("OutputDiv").innerHTML = "<p>Bla bla</p>"
 }
