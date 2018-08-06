de2425ad3fd0644ef95d91d2703854c5f1294d82
Herman Ronk <github@hermanronk.nl>
2018-08-06 07:54:17 +0200

    Basis aangemaakt

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>AFAS Profit authorisatie key</title>
+        <script src="./base.js?<?= filemtime('./base.js')?>"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    </head>
+    <body>
+        Hallo!
+    </body>
+</html>
